3e948755880c0c455aa95c60e7b7e0ce5fe7ca2e
Matthew Tesfaldet <tesfaldet@hotmail.com>
2014-07-23 23:30:42 -0400

    Added albums server controller.

---
+++ controllers/albums.js
@@ -0,0 +1,39 @@
+var models = require('../app/models'),
+
+module.exports = {
+    index: function(req, res) {
+        models.Album.find({}, function(err, data) {
+            res.json(data);
+        });
+    },
+    getById: function(req, res) {
+        models.Album.find({ _id: req.params.id }, function(err, album) {
+            if (err) {
+                res.json({error: 'Album not found.'});
+            } else {
+                res.json(album);
+            }
+        });
+    },
+    add: function(req, res) {
+        var newAlbum = new models.Album(req.body);
+        newAlbum.save(function(err, album) {
+            if (err) {
+                res.json({error: 'Error adding album.'});
+            } else {
+                res.json(album);
+            }
+        });
+    },
+    delete: function(req, res) {
+        models.Album.findOne({ _id: req.params.id }, function(err, album) {
+            if (err) {
+                res.json({error: 'Album not found.'});
+            } else {
+                album.remove(function(err, album){
+                    res.json(200, {status: 'Success'});
+                })
+            }
+        });
+    }
+};
