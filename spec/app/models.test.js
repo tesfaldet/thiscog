651dac3ece71029483a4493626d23ec2e34ecc04
Matthew Tesfaldet <tesfaldet@hotmail.com>
2014-07-24 00:13:47 -0400

    Added Mocha tests for models and added spec helper for tests.

---
+++ spec/app/models.test.js
@@ -0,0 +1,40 @@
+/* jshint -W030 */
+var models = require('../../app/models');
+
+describe('Models', function() {
+
+    describe('Album', function() {
+        var schema = models.Album.schema.paths;
+
+        it('should exist', function() {
+            expect(models.Album).to.exist;
+        });
+
+        it('should have artist string field', function() {
+            expect(schema.artist).to.exist;
+            expect(schema.artist.instance).to.equal('String');
+        });
+
+        it('should have title string field', function() {
+            expect(schema.title).to.exist;
+            expect(schema.title.instance).to.equal('String');
+        });
+
+        it('should have year string field', function() {
+            expect(schema.year).to.exist;
+            expect(schema.year.instance).to.equal('String');
+        });
+
+        it('should have genre string field', function() {
+            expect(schema.genre).to.exist;
+            expect(schema.genre.instance).to.equal('String');
+        });
+
+        it('should have cover fields'), function() {
+            expect(schema['cover.data']).to.exist;
+            expect(schema['cover.data'].instance).to.equal('Buffer');
+            expect(schema['cover.contentType']).to.exist;
+            expect(schema['cover.contentType'].instance).to.equal('String');
+        });
+    });
+});
