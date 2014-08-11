c5c4b8552b6919bb21d705d4af1cf13a10bb0986
Matthew Tesfaldet <tesfaldet@hotmail.com>
2014-08-10 22:20:36 -0400

    Updated app.js with some stuff.

--- client/src/app.js
+++ client/src/app.js
@@ -11,5 +11,27 @@ App.prototype.start = function() {
 
 	App.core.on("initialize:before", function(options) {
 		App.core.vent.trigger('app:log', 'App: Initializing');
+
+		App.views = {};
+		App.data = {};
+
+		// load initial data
+		var albums = new AlbumsCollection();
+		albums.fetch({
+			success: function() {
+				App.data.albums = albums;
+				App.core.vent.trigger('app:start');
+			}
+		});
+	});
+
+	App.core.vent.bind('app:start', function(options) {
+		App.core.vent.trigger('app:log', 'App: Starting');
+		if (Backbone.history) {
+			App.controller = new Controller();
+			App.router = new Router({ controller: App.controller });
+			App.core.vent.trigger('app:log', 'App: Backbone.history starting');
+			Backbone.history.start();
+		}
 	});
 }
